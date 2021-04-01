d7f705f8305f34f4d2c4e19ace13c996a2970549
yavkina <yavkina.nastya88@gmail.com>
2021-04-01 12:39:50 +0300

    add change

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-// валидация работает, но не выводится предупреждение confirm
+
 let salary = 0;
 let summSalary = 0;
-let averagesalary = 0;
+let averageSalary = 0;
+let historySalary = '';
 let index = 0;
+
 while (true) {
-
-  salary = +prompt("Ваша зп?", ''); index++;
-if (typeof salary === String || typeof salary === null) {
-   userAnsw = confirm ('Вы ввели не число!');{
-        if (userAnsw === false) {
-            break;
-    } else {
-        continue;
-    }
-}
-}
-  if (!salary) break;
-
+  salary = prompt(getInfo(summSalary, averageSalary, historySalary) + '\nВаша зп?'); index++;
+  if (salary === null) {
+      if (confirm ('Вы хотите выйти?')) {
+           break;
+      }
+  }
+  if (isNaN(Number(salary)) || salary === '') {
+            if (confirm('Вы допустили ошибку! Хотите выйти?')) {
+                break;
+            }
+            continue;
+        }
+  salary = Number(salary);
   summSalary += salary;
-  averagesalary = summSalary / index;
+  averageSalary = summSalary / index;
+  historySalary += salary + ' руб. ';
 }
 
-alert( 'Общая сумма: ' + summSalary + '. Средняя зп: ' + averagesalary);
+alert(getInfo(summSalary, averageSalary, historySalary));
+
+function getInfo(summSalary, averageSalary, historySalary) {
+    return `Сумма: ${summSalary}; \nСредняя: ${averageSalary}; \nИстория: ${historySalary}`;
+}
